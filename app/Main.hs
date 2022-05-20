3038d4a78b0f858b130ea468d5240e58214badad
residenti <n.queue.r@gmail.com>
2022-05-20 20:23:05 +0900

    print random food category

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import System.Random
+
+categories :: [Char]
+categories = ['和', '洋', '中']
+
+categoriesLength :: Int
+categoriesLength = length categories
 
 main :: IO ()
-main = someFunc
+main = do
+  index <- randomRIO (0, (categoriesLength - 1))
+  let category = (categories !! index)
+  putChar category
+  putStrLn "" -- TODO: 改行のために実装しているが消したい
