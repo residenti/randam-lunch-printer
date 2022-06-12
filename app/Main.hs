66fea61218aa8eecfec563f8317f947f15976974
residenti <n.queue.r@gmail.com>
2022-06-12 22:47:13 +0900

    Use Data.List.intercalate

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,9 @@
 module Main where
 
+import qualified Data.List as L
 import qualified System.Random as R
 import qualified Codec.Binary.UTF8.String as U8Str
 import qualified LunchCategoryScraper as LCS
-
-join :: [String] -> String -> String
-join [] _ = ""
-join (x:xs) partition = x ++ partition ++ join xs partition
-
-toCsv :: [String] -> String
-toCsv xs = join xs ","
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
@@ -21,7 +15,7 @@ main = do
   -- TODO: フラグでスクレイピングとプリント処理を切り分けられるようにする
   -- maybeCategories <- LCS.lunchCategories
   -- let categories = maybe [] id maybeCategories
-  -- writeFile "categories.csv" $ U8Str.decodeString $ toCsv categories
+  -- writeFile "categories.csv" $ U8Str.decodeString $ L.intercalate "," categories
 
   categoriesString <- readFile "categories.csv"
   let categories = wordsWhen (==',') categoriesString
