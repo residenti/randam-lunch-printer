c2cb0ce45fb09cda57a0319279be71f1df963407
residenti <n.queue.r@gmail.com>
2022-06-12 22:46:06 +0900

    Scraping results are now output to CSV files

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,28 @@ import qualified System.Random as R
 import qualified Codec.Binary.UTF8.String as U8Str
 import qualified LunchCategoryScraper as LCS
 
+join :: [String] -> String -> String
+join [] _ = ""
+join (x:xs) partition = x ++ partition ++ join xs partition
+
+toCsv :: [String] -> String
+toCsv xs = join xs ","
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
 main :: IO ()
 main = do
-  maybeCategories <- LCS.lunchCategories -- TODO 都度スクレイピングするの止める
-  let categories = maybe [] id maybeCategories
+  -- TODO: フラグでスクレイピングとプリント処理を切り分けられるようにする
+  -- maybeCategories <- LCS.lunchCategories
+  -- let categories = maybe [] id maybeCategories
+  -- writeFile "categories.csv" $ U8Str.decodeString $ toCsv categories
+
+  categoriesString <- readFile "categories.csv"
+  let categories = wordsWhen (==',') categoriesString
   let categoriesLength = length categories
   index <- R.randomRIO (0, (categoriesLength - 1))
   let category = (categories !! index)
-  putStrLn $ U8Str.decodeString category
+  putStrLn category
