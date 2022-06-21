ae24624f228ad4a6c8c350f6504a16e5bd05f806
residenti <n.queue.r@gmail.com>
2022-06-21 21:36:34 +0900

    Eliminated --setup option

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import qualified Data.List as L
-import qualified System.Environment as E
 import qualified System.Directory as D
 import qualified System.Random as R
 import qualified Codec.Binary.UTF8.String as U8Str
@@ -13,27 +12,35 @@ wordsWhen p s =  case dropWhile p s of
                       "" -> []
                       s' -> w : wordsWhen p s''
                             where (w, s'') = break p s'
+
+prepareConfigDirPath :: IO FilePath
+prepareConfigDirPath = do
+  homePath <- D.getHomeDirectory
+  return $ homePath ++ "/" ++ ".lunch"
+
+prepareCategoryListPath :: IO FilePath
+prepareCategoryListPath = do
+  configDirPath <- prepareConfigDirPath
+  return $ configDirPath ++ "/" ++ "categories.csv"
+
 main :: IO ()
 main = do
-  homePath <- D.getHomeDirectory
-  let configDirPath = homePath ++ "/" ++ ".lunch"
-  let categoryListPath = configDirPath ++ "/" ++ "categories.csv"
+  categoryListPath <- prepareCategoryListPath
+  hasCategoryList <- D.doesFileExist categoryListPath
 
-  args <- E.getArgs
-  let hasSetupOption = length args > 0 && head args == "--setup"
+  if not hasCategoryList then do
+    putStrLn "Start creating Lunch Category List..."
+    maybeCategories <- LCS.lunchCategories -- NOTE: もはやcategories.csvを準備しといて、それをコピーすればスクレイピングの必要ない気が。。。
+    let categories = maybe [] id maybeCategories
+    configDirPath <- prepareConfigDirPath
+    D.createDirectoryIfMissing False configDirPath
+    writeFile categoryListPath $ U8Str.decodeString $ L.intercalate "," categories
+    putStrLn "Completed!!"
+  else return()
 
-  if hasSetupOption
-    then do
-      putStrLn "Start creating Lunch Category List..."
-      maybeCategories <- LCS.lunchCategories -- NOTE: もはやcategories.csvを準備しといて、それをコピーすればスクレイピングの必要ない気が。。。
-      let categories = maybe [] id maybeCategories
-      D.createDirectoryIfMissing False configDirPath
-      writeFile categoryListPath $ U8Str.decodeString $ L.intercalate "," categories
-      putStrLn "Completed!!"
-    else do
-      categoriesString <- readFile categoryListPath
-      let categories = wordsWhen (==',') categoriesString
-      let categoriesLength = length categories
-      index <- R.randomRIO (0, (categoriesLength - 1))
-      let category = (categories !! index)
-      putStrLn $  "イタダキマ～ス♪(●人´∀｀) ＼" ++ category ++ "／"
+  categoriesString <- readFile categoryListPath
+  let categories = wordsWhen (==',') categoriesString
+  let categoriesLength = length categories
+  index <- R.randomRIO (0, (categoriesLength - 1))
+  let category = (categories !! index)
+  putStrLn $  "イタダキマ～ス♪(●人´∀｀) ＼" ++ category ++ "／"
