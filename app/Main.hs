c2b2d4398133ab56fc4c0b9ded24eea3728286b9
residenti <n.queue.r@gmail.com>
2022-06-24 01:30:56 +0900

    feature to randomly print one restaurant within 1,000 meters of a specified latitude and longitude

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,27 @@
 module Main where
 
 
+import qualified Data.Aeson as A
+import qualified System.Posix.Env.ByteString as E
 import qualified System.Directory as D
 import qualified System.Random as R
 import qualified RandamLunchPrinter as RLP
+import qualified Network.HTTP.Simple as HS
+import qualified Data.ByteString.Char8 as BC
 
+hasShopOption :: [BC.ByteString] -> Bool
+hasShopOption args = length args > 0 && head args == "--s"
 
+latLon :: [BC.ByteString] -> RLP.LatLon
+latLon args = RLP.LatLon { RLP.latitude = lat, RLP.longitude = lon  }
+  where
+    lat = args !! 1
+    lon = args !! 2
+
+-- TODO 緯度経度を設定ファイルから読み取るようにする
 main :: IO ()
 main = do
-  categoryListPath <- RLP.prepareCategoryListPath
-  hasCategoryList <- D.doesFileExist categoryListPath
-
-  if not hasCategoryList then do
-    putStrLn "Start creating Lunch Category List..."
-    configDirPath <- RLP.prepareConfigDirPath
-    D.createDirectoryIfMissing False configDirPath
-    D.copyFile "categories.csv" categoryListPath
-    putStrLn "Completed!!"
-  else return()
-
-  categoriesString <- readFile categoryListPath
-  let categories = RLP.wordsWhen (==',') categoriesString
-  let categoriesLength = length categories
-  index <- R.randomRIO (0, (categoriesLength - 1))
-  let category = (categories !! index)
-  putStrLn $ "イタダキマ～ス♪(●人´∀｀) ＼" ++ category ++ "／"
+  args <- E.getArgs
+  if hasShopOption args
+  then RLP.putShopName $ latLon args
+  else RLP.putLunchCategoryName
